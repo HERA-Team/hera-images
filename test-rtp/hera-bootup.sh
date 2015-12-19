b14c3ebe740792141ea497d5af4e88f0336bfaaf
Peter Williams <peter@newton.cx>
2015-12-19 12:15:37 -0500

    test-rtp: these also need to run sshd
    TODO: set up passwordless keys and known host keys so everyone can SSH everywhere automagically.

--- test-rtp/hera-bootup.sh
+++ test-rtp/hera-bootup.sh
@@ -5,4 +5,8 @@
 
 set -e
 sed -e "s/@DB_PASSWORD@/$HERA_DB_PASSWORD/g" </hera/rtp/etc/still.cfg.tmpl >/hera/rtp/etc/still.cfg
-exec /hera/rtp/bin/still.py --server
+
+# Meh, just run this in the background.
+/usr/sbin/sshd -D &
+
+exec /hera/rtp/bin/still.py "$@"
