eef05f2257239e6e7064d52b6c9d7133ff0330e3
Peter Williams <peter@newton.cx>
2016-01-27 14:00:42 -0500

    ssh-stack: streamline Dockerfile here too

---
+++ ssh-stack/setup.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Copyright 2016 the HERA Collaboration
+# Licensed under the MIT License.
+
+set -e -x
+
+mkdir -p /var/run/sshd /root/.ssh /etc/ssh
+
+cp /setup/ssh_host_rsa_key /etc/ssh/
+cp /setup/ssh_host_rsa_key.pub /etc/ssh/
+rm -f /etc/ssh/ssh_host_dsa* /etc/ssh/ssh_host_ecdsa*
+chmod 600 /etc/ssh/ssh_host*
+
+cp /setup/insecure_id_rsa /root/.ssh/id_rsa
+cp /setup/insecure_id_rsa.pub /root/.ssh/id_rsa.pub
+cp /setup/insecure_id_rsa.pub /root/.ssh/authorized_keys
+cp /setup/ssh_host_rsa_key.pub /root/.ssh/known_hosts
+echo -n '* ' >/root/.ssh/known_hosts
+cat /etc/ssh/ssh_host_rsa_key.pub >>/root/.ssh/known_hosts
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/*
+
+# Self-destruct!
+cd /
+rm -rf /setup/
